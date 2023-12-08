5177b717eae1f5168db59f2ee4c23ee2bf36c79b
akp10110 <akp10110@gmail.com>
2023-12-08 15:49:44 +1100

    Added the initial sudoku to solve

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 def sudoku():
-    arr = [[1,1,10,1,1],
-           [2,2,2,2,2],
-           [3,3,3,3,3,3],
-           [4,4,4,4,4,],
-           [5,5,5,5,5,]]
-    print(arr)
-    print(arr[0])
-    print(arr[0][2])
+    sudoko_initial_to_solve = [
+        [6,0,0,8,0,3,9,0,7],
+        [2,0,8,0,0,9,0,5,0],
+        [3,0,0,1,2,0,0,8,0],
+        [0,0,0,4,9,0,3,0,5],
+        [0,0,0,0,0,0,0,0,0],
+        [9,0,3,0,1,2,0,0,0],
+        [0,3,0,0,6,1,0,0,8],
+        [0,2,0,9,0,0,5,0,1],
+        [1,0,4,5,0,8,0,0,9]
+        ]
+    
+    for row in range(len(sudoko_initial_to_solve)):
+        # print each column
+        print(sudoko_initial_to_solve[row])
     
 sudoku()
